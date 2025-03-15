88811004ce90814d21e958a339eafe21223ccf16
soetko <145643562+soetko@users.noreply.github.com>
2025-03-15 20:16:27 +0630

    Update app.py

--- 3-2-view-functions/starter/app.py
+++ 3-2-view-functions/starter/app.py
@@ -11,5 +11,10 @@ posts = {
 def home():
     return '<h1>Welcome to My Blog</h1><p>Click on the posts to learn more about Flask.</p>'
 
+@app.route("/post/1")
+def show_post():
+    post = posts[1]
+    return f"<h1> {post['title']} </h1> <p> {post['content']} </p>"
+
 if __name__ == '__main__':
     app.run(debug=True)
