43715a910603079f9190e7a48d556482cbcab65e
soetko <145643562+soetko@users.noreply.github.com>
2025-03-15 19:57:53 +0630

    Update app.py

--- 2-3-project-app-structure/starter/app.py
+++ 2-3-project-app-structure/starter/app.py
@@ -1,5 +1,13 @@
 #import the Flask class and create an instance of it
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 #define homepage route and index() view function
+@app.route('/')
+def index():
+  return render_template("index.html")
 
 #run the app in debug mode
+if __name__ == "__main__":
+  app.run(debug=True)
