8ba55c809dc722b029d4a57786f80122313e43e8
soetko <145643562+soetko@users.noreply.github.com>
2025-03-15 20:24:33 +0630

    Update app.py

--- 3-3-url-parameters/starter/app.py
+++ 3-3-url-parameters/starter/app.py
@@ -11,9 +11,11 @@ posts = {
 def home():
     return '<h1>Welcome to My Blog</h1><p>Click on the posts to learn more about Flask.</p>'
 
-@app.route('/post/1')
-def show_post():
-    post = posts[1]
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return "<h1>404: Post Not Found</h1>"
     return f"<h1>{post['title']}</h1><p>{post['content']}</p>"
 
 if __name__ == '__main__':
