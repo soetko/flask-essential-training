16781643f604ab790e1da6d77b23727f2b769764
soetko <145643562+soetko@users.noreply.github.com>
2025-03-16 21:02:12 +0630

    update 5-4

--- 5-4-project-form/starter/app.py
+++ 5-4-project-form/starter/app.py
@@ -1,21 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
+from forms import HealthDataForm
 
 app = Flask(__name__)
+app.secret_key = 'you_will_never_guess'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
+    form = HealthDataForm()
     if request.method == 'POST':
         # Process form data here
         return redirect(url_for('dashboard'))
-    return render_template('form.html')
+    return render_template('form.html', form=form)
+
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
