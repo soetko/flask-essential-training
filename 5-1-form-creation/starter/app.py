b71793a30502279e67e03a37b0ee3d24519dd38a
soetko <145643562+soetko@users.noreply.github.com>
2025-03-16 19:50:25 +0630

    update 5-1

--- 5-1-form-creation/starter/app.py
+++ 5-1-form-creation/starter/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from forms import MyForm
 
 app = Flask(__name__)
+app.secret_key = "supersecterkey"
 
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = MyForm()
+    if form.validate_on_submit():
+        return f"Hello, {form.name.data}!"
+    return render_template("form.html", form=form)
 
 
 if __name__ == '__main__':
