f3e7042e318487e849c57684584ec086b03a910a
soetko <145643562+soetko@users.noreply.github.com>
2025-03-16 20:36:00 +0630

    update 5-3

--- 5-3-form-submission/starter/app.py
+++ 5-3-form-submission/starter/app.py
@@ -4,14 +4,16 @@ from forms import MyForm
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = MyForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         username = form.username.data
         email = form.email.data
-        return f"Hello, {username}! We’ve received your email: {email}."
+        return render_template("success.html", username=username, email=email)
     return render_template('form.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
