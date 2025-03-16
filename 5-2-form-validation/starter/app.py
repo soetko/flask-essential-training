0c849824af73792a46986bdddc8f113ce51bc1a7
soetko <145643562+soetko@users.noreply.github.com>
2025-03-16 20:24:39 +0630

    update 5-2

--- 5-2-form-validation/starter/app.py
+++ 5-2-form-validation/starter/app.py
@@ -7,8 +7,10 @@ app.secret_key = 'supersecretkey'
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = MyForm()
-    if request.method == 'POST':
-        return f"Hello, {form.username.data}!"
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        return f"Hello, {username}! We've received your email: {email}."
     return render_template('form.html', form=form)
 
 if __name__ == '__main__':
--- 5-2-form-validation/starter/forms.py
+++ 5-2-form-validation/starter/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
+
 
 class MyForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
