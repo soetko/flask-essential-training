da549a7502d2d1d8e3b067b6d3609bc53900949d
soetko <145643562+soetko@users.noreply.github.com>
2025-03-18 16:05:33 +0630

    update 6-4

--- 6-4-db-create-and-read/starter/app.py
+++ 6-4-db-create-and-read/starter/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import MyForm
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  
+app.secret_key = 'supersecretkey'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_tracker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,15 +19,34 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = MyForm()
     if request.method == 'POST' and form.validate_on_submit():
         username = form.username.data
         email = form.email.data
-        return render_template('success.html', username=username, email=email)
+
+        new_user = User(username=username, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('success', username=username, email=email))
     return render_template('form.html', form=form)
+
+
+@app.route("/success")
+def success():
+    username = request.args.get("username")
+    email = request.args.get("email")
+    return render_template("success.html", username=username, email=email)
+
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template("users.html", users=all_users)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
