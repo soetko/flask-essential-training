69f97cd27a0407bd8c479b20488498bdf4f9fc01
soetko <145643562+soetko@users.noreply.github.com>
2025-03-18 15:09:44 +0630

    update 6-1

--- 6-1-db-setup/starter/app.py
+++ 6-1-db-setup/starter/app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template, request
 from forms import MyForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  
+app.secret_key = 'supersecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_tracker.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/form', methods=['GET', 'POST'])
 def form():
@@ -13,6 +19,6 @@ def form():
         return render_template('success.html', username=username, email=email)
     return render_template('form.html', form=form)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
