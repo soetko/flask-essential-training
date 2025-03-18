4e9cc0fdaf5ad8b44387b287296765f2772a8239
soetko <145643562+soetko@users.noreply.github.com>
2025-03-18 16:59:11 +0630

    update 6-6

--- 6-6-db-update-and-delete/starter/app.py
+++ 6-6-db-update-and-delete/starter/app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import MyForm
 
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  
+app.secret_key = 'supersecretkey'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///health_tracker.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -18,6 +19,7 @@ class User(db.Model):
     def __repr__(self):
         return f'<User {self.username}>'
 
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     form = MyForm()
@@ -26,25 +28,44 @@ def form():
         email = form.email.data
 
         new_user = User(username=username, email=email)
-        db.session.add(new_user) # stage the new user for addition
-        db.session.commit() # save the new user to the database
+        db.session.add(new_user)  # stage the new user for addition
+        db.session.commit()  # save the new user to the database
 
         return redirect(url_for('success', username=username, email=email))
     return render_template('form.html', form=form)
 
+
 @app.route('/success')
 def success():
-    #retrieve username and email from the URL query string
-    username = request.args.get('username') 
+    # retrieve username and email from the URL query string
+    username = request.args.get('username')
     email = request.args.get('email')
     return render_template('success.html', username=username, email=email)
 
-@app.route('/users')
+
+@app.route('/users', methods=["POST", "GET"])
 def users():
-    #retrieve all records from the User table in the db
+    if request.method == "POST":
+        user_id = request.args.get("user_id")
+        new_email = request.args.get("new_email")
+        user_to_update = User.query.get(user_id)
+        if user_to_update:
+            user_to_update.email = new_email
+            db.session.commit()
+        return (redirect(url_for("users")))
+    # retrieve all records from the User table in the db
     all_users = User.query.all()
     return render_template('users.html', users=all_users)
 
+
+@app.route('/delete_user/<int:id>', methods=['POST'])
+def delete_user(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('users'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-
